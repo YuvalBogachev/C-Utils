5ce7ee65f0953ab5f936ac5b1833aa1dc5e2627d
Yuval Bogachev <code@yuvalbogachev.com>
2024-08-06 15:26:25 +0300

    Started table/ui

---
+++ src/ui/table.c
@@ -0,0 +1,7 @@
+void draw_table(char*** data, int columns, int* lens, int* sizes) {
+    for (int i = 0; i < columns; i++) {
+        for (int j = 0; j < lens[i]; j++) {
+            
+        }
+    }
+}
