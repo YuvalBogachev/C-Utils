0fd7a679fff6a96c62dab0480587ed029da3d867
Yuval Bogachev <code@yuvalbogachev.com>
2024-08-07 20:14:47 +0300

    Very minor UI code expansion

--- src/ui/table.c
+++ src/ui/table.c
@@ -1,7 +1,10 @@
 void draw_table(char*** data, int columns, int* lens, int* sizes) {
     for (int i = 0; i < columns; i++) {
         for (int j = 0; j < lens[i]; j++) {
-            
+            char row[] = "";
+            for (int k = 0; k < sizes[i]; k++) {
+                
+            }
         }
     }
 }
